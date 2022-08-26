41ea34150fd3501d929ffb45617c77cdbaf0cdf7
Prabir Tarafdar <ptarafdar010@gmail.com>
2022-08-26 23:03:41 +0530

    add controller

---
+++ BookstoreWebApp/BookstoreWebApp/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookstoreWebApp.Controllers
+{
+    public class HomeController:Controller
+    {
+        public string Index()
+        {
+            return "returning from HomeController Index action method";
+        }
+    }
+}
